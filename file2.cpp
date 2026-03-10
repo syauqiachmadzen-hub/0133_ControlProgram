20365f91dffac617f1085c61cb3ca7ca50ffeb20
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-03-10 19:41:29 +0700

    commit3

--- file2.cpp
+++ file2.cpp
@@ -4,3 +4,15 @@ using namespace std;
 float rata_rata(float a, float b){
     return (a+b)/2;
 }
+
+string status_lulus(float r){
+    if (r >= 60)
+       return "lolos";
+    else if (r >= 50)
+       return "lolos dipertimbangkan";
+    else if (r >= 40)
+       return "lolos tetapi matrikulasi 1 tahun";
+    else 
+       return "gagal";       
+
+}
