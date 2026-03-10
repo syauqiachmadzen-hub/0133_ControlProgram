1b2af14a96d00f86d61ce08aed5a3115a2053286
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-03-10 19:30:00 +0700

    commit1

---
+++ file1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x;
+
+    srand(time(0));
+    x = rand() %10;
+
+    if (x==5){
+        x = x=1;
+        cout<<"nilai x sudah di koreksi menjadi : "<<x<<endl;
+    }
+
+    cout << "bilangan yang diberikan = "<<x;
+}
